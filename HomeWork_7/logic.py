f6d1d1d17338497190c223e3325a9d6f720fb805
Maksim_Mikosha <mikmaksim@gmail.com>
2022-08-17 23:21:54 +0300

    Change HomeWork_7

--- HomeWork_7/logic.py
+++ HomeWork_7/logic.py
@@ -1,14 +1,29 @@
 import csv
 
-def fill_first_dict(dict):
+def fill_first_dict(users_dict):
+    users_dict = {"0" : ("First name", "Second name", "Number", "Note")}
+    return users_dict
+
+def fill_test_dict(dict):
     dict = {"0" : ("First name", "Second name", "Number", "Note"),
             "1" : ("Ivan", "Petrov", "4952578645", "Work number"),
             "2" : ("Sergey", "Vasechkin", "4612358476", "Work number"),
             "3" : ("Maksim", "Popov", "9782536741", "Privat number")}
     return dict
 
-def add_to_file(list):
-    pass
+def add_to_file(users_dict):
+    print(f'Заголовки каталога - {users_dict["0"]}')
+    countKeys = len(list(users_dict.keys())) + 1
+    print(countKeys)
+    tempList = list()
+    tempList.append(countKeys)
+    tempList.append(input('Введи имя -'))
+    tempList.append(input('Введи фамилию -'))
+    tempList.append(input('Введи номер телефона -'))
+    tempList.append(input('Введи заметку -'))
+    print(tempList)
+    users_dict = dict(tempList)
+    return users_dict
 
 def import_to_line(dict):
     with open("Python_HomeWork\HomeWork_7\import-variant1.csv", "w") as file:
--- HomeWork_7/main.py
+++ HomeWork_7/main.py
@@ -10,9 +10,24 @@ def view(dict):
 def main():
     catalog = {}
     catalog = logic.fill_first_dict(catalog)
-    view(menu.ui_menu())
-    logic.import_to_line(catalog)
-    logic.import_by_line(catalog)
-
+    inputCommand = 0
+    while inputCommand != 6:
+        print('Список доступных комманд!')
+        view(menu.ui_menu())
+        inputCommand = int(input('Введи номер команды: '))
+        if inputCommand == 0:
+            catalog = logic.fill_test_dict(catalog)
+            print('Вроде получилось)')
+        elif inputCommand == 1:
+            catalog = logic.add_to_file(catalog)
+        elif inputCommand == 2:
+            print('В разработке')
+        elif inputCommand == 3:
+            logic.import_by_line(catalog)
+        elif inputCommand == 4:
+            logic.import_to_line(catalog)
+        else:
+            view(catalog)
+ 
 if __name__ == "__main__":
     main()
